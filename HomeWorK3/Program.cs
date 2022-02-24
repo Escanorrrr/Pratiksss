97d770dd8fca8cdf60287d56a40c3ccb932e0c8c
Escanorrrr <ramazanay200@hotmail.com>
2022-02-24 14:07:39 +0300

    HomeWork2 Tamamlandı.

---
+++ HomeWorK3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace algoritmaSorusu3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Bir Kelime Giriniz : ");
+            int x = int.Parse(Console.ReadLine());           
+            string[] yenidizi =  new string[x];
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Lütfen {0}. Sayıyı Giriniz",i+1);
+                yenidizi[i] = Convert.ToString(Console.ReadLine());
+
+            }
+            foreach (var item in yenidizi)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+        }
+    }
+}
+
